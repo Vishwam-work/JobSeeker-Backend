c41fa7f45716d8a84279d7ee852328e2bb8aa328
Vishwam-work <vishuwork247@gmail.com>
2025-08-08 12:47:09 +0530

    Add the CRSF token in the backend live link

--- Jobseeker/settings.py
+++ Jobseeker/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://jobseeker-backend-jy1y.onrender.com"
 ]
 
 ROOT_URLCONF = 'Jobseeker.urls'
@@ -143,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
