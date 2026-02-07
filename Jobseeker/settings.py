30e9abb498a87f40da390c881f347f245286bc8a
Vishwam-work <vishuwork247@gmail.com>
2026-02-07 11:43:35 +0530

    Add the Vercel CORS url

--- Jobseeker/settings.py
+++ Jobseeker/settings.py
@@ -64,14 +64,16 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://jobseeker-backend-jy1y.onrender.com",
-    "https://jobseeker-backend-jy1y.onrender.com"
+    "https://jobseeker-backend-jy1y.onrender.com",
+    "https://jobseeker-nbt8s9yx3-vishwam-works-projects.vercel.app"
 ]
 
 # CSRF Trusted Origins for production
 CSRF_TRUSTED_ORIGINS = [
     "https://jobseeker-backend-jy1y.onrender.com",
     "http://localhost:3000",
-    "https://jobseeker-backend-jy1y.onrender.com"
+    "https://jobseeker-backend-jy1y.onrender.com",
+    "https://jobseeker-nbt8s9yx3-vishwam-works-projects.vercel.app/"
 ]
 
 ROOT_URLCONF = 'Jobseeker.urls'
