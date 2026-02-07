66e1bb3da062b1e15a27ee136b3410037852f9d8
Vishwam-work <vishuwork247@gmail.com>
2026-02-07 13:23:40 +0530

    Add the CORS URL Change

--- Jobseeker/settings.py
+++ Jobseeker/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://jobseeker-backend-jy1y.onrender.com",
     "https://jobseeker-backend-jy1y.onrender.com",
-    "https://nvglobaltestingserver-nbcnl1x67-vishwam-works-projects.vercel.app/"
+    "https://nvglobaltestingserver.vercel.app/"
 ]
 
 # CSRF Trusted Origins for production
@@ -73,7 +73,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://jobseeker-backend-jy1y.onrender.com",
     "http://localhost:3000",
     "https://jobseeker-backend-jy1y.onrender.com",
-    "https://nvglobaltestingserver-nbcnl1x67-vishwam-works-projects.vercel.app/"
+    "https://nvglobaltestingserver.vercel.app/"
 ]
 
 ROOT_URLCONF = 'Jobseeker.urls'
