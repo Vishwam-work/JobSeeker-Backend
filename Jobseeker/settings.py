0fe1589a00652b57c8bab0c239a34de3771bad0b
Vishwam-work <vishuwork247@gmail.com>
2026-04-21 12:29:51 +0530

    ADD the test url of employeer side new

--- Jobseeker/settings.py
+++ Jobseeker/settings.py
@@ -66,8 +66,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3005",
     "https://jobseeker-backend-jy1y.onrender.com",
     "https://ngblobaltestingserver60.vercel.app",
-    "http://192.168.1.9:3000",
-    "http://192.168.1.3:3005"
+    "http://10.194.205.163:3005",
+    "http://10.194.205.163:3000"
 ]
 
 # CSRF Trusted Origins for production
@@ -77,8 +77,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3005",
     "https://jobseeker-backend-jy1y.onrender.com",
     "https://ngblobaltestingserver60.vercel.app",
-    "http://192.168.1.9:3000",
-    "http://192.168.1.3:3005"
+    "http://10.194.205.163:3005",
+    "http://10.194.205.163:3000"
 ]
 
 ROOT_URLCONF = 'Jobseeker.urls'
